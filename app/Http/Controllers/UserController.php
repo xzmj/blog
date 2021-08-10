b5d42207a6d2e0ff5ea2b7016d0054d0b3d5326f
xzmj <1029750048@qq.com>
2021-08-11 02:39:35 +0800

    8.112

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -101,7 +101,7 @@ class UserController extends Controller
             $pdf = PDF::loadView('user.resume');
             // download pdf
             $pdf->download('李峻峰简历.pdf');
-//        return view('user.resume');
+        return view('user.resume');
 
     }
 
